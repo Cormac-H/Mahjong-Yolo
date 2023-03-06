179423614eea0f3f0370da608b2b50f79507f7d3
Cormac H <cormach4@gmail.com>
2023-03-06 19:56:26 +1100

    Evaluated model on test images and videos

--- Main.py
+++ Main.py
@@ -3,14 +3,26 @@ from IPython.display import Image
 import torch
 import sys
 
-def main():
-    print(sys.prefix)
+BEST_MODEL = "Current_Best_Model.pt"
+DEFAULT_MODEL = "yolov8n.pt"
 
-    model = YOLO("yolov8n.pt")
+def trainModel():
+    model = YOLO(DEFAULT_MODEL)
     model.train(data="MahjongTiles.yaml", epochs=3, cache=True)
     metrics = model.val()
     print(metrics)
-    print("done")
+
+def predictOnBestModel():
+    model = YOLO(BEST_MODEL)
+    results = model.predict(source="Test-Screenshots", save=True)
+    model.predict(source="Test-Videos", save=True)
+    print(results.boxes)
+    print(results.probabilities)
+    
+def main():
+    # torch.cuda.is_available = lambda : False
+    # predictOnBestModel()
     return
+
 
 if __name__ == "__main__": main()
